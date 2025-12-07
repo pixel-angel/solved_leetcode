fad06c42e41464959631564420a23a2c090ea7d3
pixel-angel <alisha85as@gmail.com>
2025-12-07 18:30:04 +0530

    7.12.25

---
+++ easy/1523.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        if (low%2==0){
+            if (high%2==0)
+                return (high-low)/2;
+            return (high-low+1)/2;
+        }
+        if (high%2==0)
+            return (high-low+1)/2;
+        return (high-low+2)/2;
+    }
+    
+};

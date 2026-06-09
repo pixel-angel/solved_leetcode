cf704fe701443fe39956d947125e6810eaf75d79
pixel-angel <alisha85as@gmail.com>
2026-06-09 23:45:07 +0530

    9.6.26

---
+++ medium/3689.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int a = *max_element(nums.begin(), nums.end());
+        int b = *min_element(nums.begin(), nums.end());
+        return 1LL * (a - b) * k;        
+    }
+};
